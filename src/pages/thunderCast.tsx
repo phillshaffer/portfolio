9ad9e8844d72725d08702b9dba4f1a207dbfb997
phillshaffer <phill@phillshaffer.com>
2020-11-18 22:07:39 -0800

    New Thundercast story structure in place.

--- src/pages/thunderCast.tsx
+++ src/pages/thunderCast.tsx
@@ -11,6 +11,7 @@ import { Image } from '../components/image'
 import { ImageGallery } from '../components/imageGallery'
 
 // assests
+import ThunderCastBrainstorm from '../images/thunderCast/ThunderCastBrainstorm.png'
 import ElevatorPitch from '../images/thunderCast/ElevatorPitch@2x.png'
 import ThunderCast_InitalWireframes from '../images/thunderCast/ThunderCast_InitalWireframes.png'
 import ThunderCast_FinalWireframes from '../images/thunderCast/ThunderCast_FinalWireframes.png'
@@ -53,55 +54,128 @@ export const ThunderCast = (props: ThunderCastProps) => {
     <React.Fragment>
       <GlobalNav isBack={true} />
       <Styled_Main>
+        
         <Section id="a" backgroundColor="#222222">
-          <Text font="headline">Extending Ad Creative to Social Ad Platforms</Text>
+          <Text font="headline">Google Design Sprint</Text>
           <Text font="normal">
-            ThunderCast was the culmination of months of work to establish a new ad standardization to scale display ads across web and social ad platforms. This standardization was cemented within the Thunder Ad Framework, backend data model, ThunderKit RESTful API and ThunderCast mobile app designed to facilitate real time ad design collaboration. Together these projects proved the ad framework’s scalability, with the mobile app proving technology interoperability with newly created API and backend.
+            Blank
           </Text>
         </Section>
 
         <Section id="a" backgroundColor="#333333">
           <Text font="subtitle">The Challenge</Text>
-          <Text font="headline">Scale Thunder’s Display Ad Technology to Social ad platforms</Text>
+          <Text font="headline">Build Thunder’s first Mobile Application </Text>
           <Text font="normal">
-            Thunder Industries had transformed the way in which marketing teams design and scale ad creative for web display ads. The central selling point of their creative studio product, was build once and scale ad designs across multiple ad sizes with ease. As Thunder grew so did the sophistication of their customers. No longer where users simply creating one off ads, they were designing long running sophisticated marketing campaigns that comprised many ads with personalized design variations. At the same time, ad buying was trending more and more to mobile and social ad platforms. For Thunder to stay relevant, they has to extend their offering beyond web display ads.
+            Blank
           </Text>
-          <Text font="normal">
-            Over a series of interconnected high profile projects, various feature crews would tackle the following business objectives.
+          <Text font="title">
+            The BIG Challenge          
           </Text>
           <List>
-            <Item>Scale ad creative across web, mobile and social ad platforms.</Item>
-            <Item>Better Integrate with industry partners to create more platform added value.</Item>
-            <Item>Expand platform products and tools beyond the web.</Item>
+            <Item>blank</Item>
           </List>
-          <Text font="title">My Role</Text>
-          <Text font="normal">
-            Over the coming months, I would lead in both a product management and product design capacity a series of high profile initiatives to standardize our approach to ad creative, improve ad creative storage and access, and developed the business case and designed the company’s first mobile app.
+          <Text font="title">
+            The Team         
           </Text>
-          <Image src={ElevatorPitch} isPadding={false} />
+          <List>
+            <Item>blank</Item>
+          </List>
+          <Text font="title">
+            The Role         
+          </Text>
+          <List>
+            <Item>blank</Item>
+          </List>
+          {/* <Image src={ElevatorPitch} isPadding={false} /> */}
         </Section>
 
         <Section id="a" backgroundColor="#222222">
-          <Text font="subtitle">ThunderCast Elevator Pitch</Text>
-          <Text font="headline">Real Time Ad Design and Collaboration</Text>
+          <Text font="subtitle">Day 1 - Monday</Text>
+          <Text font="headline">Setting the Stage</Text>
           <Text font="normal">
-            Thunder Cast is a mobile application that facilitates real time ad design collaboration for today’s top brands. While on the go or in between meetings, brand managers, creative directors, designers and other team collaborators can easily preview and comment on submitted Ad designs. When an ad design is ready for feedback, simply submit an ad for review from Thunder Creative Studio. Whether responding to a notification or leaving feedback, our intuitive user experience enables teams to cycle through each size of the final ad design. With a simple flip, teams can review ad designs across web and social media platforms. With the power of Thunder Cast everyone on the team can participate, enabling today’s marketing teams to finalize ad designs faster than ever.
+            Blank
+          </Text>
+          <Text font="normal">
+            Blank
           </Text>
           <Text font="title">
-            User Goals
+            Long Term Goal
           </Text>
           <List>
-            <Item>View submitted ad designs on a mobile device.</Item>
-            <Item>Leave feedback on ad designs.</Item>
-            <Item>Provide ad approval outside creative studio.</Item>
+            <Item>Blank</Item>
           </List>
         </Section>
 
         <Section id="a" backgroundColor="#333333">
-          <Text font="subtitle">The Approach</Text>
-          <Text font="headline">Real Time Ad Design and Collaboration</Text>
+          <Text font="subtitle">Day 2 - Tuesday</Text>
+          <Text font="headline">Lightning Demos</Text>
           <Text font="normal">
-            With our initial set of user goals in hand, I led the feature crew to define the set of user tasks required to satisfy our identified goals. Additionally we had a business directive to have a proof of concept designed, coded and deployable to internal stakeholders within two weeks. This shortened timeline meant we had to be very disciplined in defining our core user tasks and subsequent user experience.
+            Blank
+          </Text>
+          <Text font="normal">
+            Blank
+          </Text>
+          <Text font="title">
+            Previewing Ad Creative
+          </Text>
+          <Text font="normal">
+            Blank
+          </Text>
+          <Text font="normal">
+            Blank
+          </Text>
+          <Text font="title">
+            Contextual Ad Comments
+          </Text>
+          <Text font="normal">
+            Blank
+          </Text>
+          <Text font="title">
+            Brainstorming
+          </Text>
+          <Text font="normal">
+            Blank
+          </Text>
+          <Image src={ThunderCastBrainstorm} />
+        </Section>
+
+
+        <Section id="a" backgroundColor="#222222">
+          <Text font="subtitle">Day 3 - Wednesday</Text>
+          <Text font="headline">The Rumble</Text>
+          <Text font="normal">
+            Blank
+          </Text>
+          <Text font="title">
+            Solution A
+          </Text>
+          <Image src={ThunderCast_InitalWireframes} />
+          <Text font="normal">
+            Blank
+          </Text>
+          <Text font="normal">
+            Blank
+          </Text>
+          <Text font="normal">
+            Blank
+          </Text>
+          <Text font="title">
+            Solution B
+          </Text>
+          <Image src={ThunderCast_FinalWireframes} />
+          <Text font="normal">
+            Blank
+          </Text>
+          <Text font="normal">
+            Blank
+          </Text>
+        </Section>
+
+        <Section id="a" backgroundColor="#333333">
+          <Text font="subtitle">Day 4 - Thursday</Text>
+          <Text font="headline">Defining the Prototype</Text>
+          <Text font="normal">
+            Blank
           </Text>
           <Text font="title">
             User Tasks
@@ -117,39 +191,45 @@ export const ThunderCast = (props: ThunderCastProps) => {
             <Item>Ability to navigate between ad units for a selected ad design.</Item>
           </List>
           <Text font="title">
-            Core Workflows and App Navigation
+            Divide and Conquer
+          </Text>
+          <Text font="title">
+            User Tasks
+          </Text>
+        </Section>
+
+        <Section id="a" backgroundColor="#222222">
+          <Text font="subtitle">Day 5 - Friday</Text>
+          <Text font="headline">ThunderCast Big Reveal</Text>
+          <Text font="normal">
+            Blank
+          </Text>
+          <Text font="title">
+            Elevator Pitch
           </Text>
           <Text font="normal">
-            We started our design process by first mapping our core workflows and app navigation.
+            Blank
           </Text>
-          <Image src={ThunderCast_InitalWireframes} />
-          <Text font="normal">
-            As we started to dive deeper into our initial wireframing two key user challenges emerged.
-          </Text>
-          <Text font="normal">
-            First when leaving comments on a given ad unit, users told us they wanted to make those comments contextual. For example they wanted to flag a given area of an ad design and add comments similar to sticking a post-it note to a printed version of the ad. As a result we modified our designs to make comments less chat focussed and went all in on contextual commenting. This learning also led to comments being designed as a mode, vs. a separate screen within the app.          
+          <Text font="title">
+            Reception
           </Text>
           <Text font="normal">
-            Secondly users found it laborious to go back and forth between ad preview and the ad unit gallery to select a new ad unit to preview. We attempted to overlay the ad unit selection within the ad preview itself, but this solution competed with the ad design preview and didn’t significantly reduce ad unit selection fatigue. Initially when designing the gallery we thought it would be important for users to see all the available ad units for a given ad design in one place. In actuality ad design collaborators told us it was more important to them to quickly skim between ad units and make comments as required. This feedback led us to implement a swipe up or flipping interaction to navigate between ad units.
+            Blank
           </Text>
-          <Image src={ThunderCast_FinalWireframes} />
         </Section>
 
         <Section id="a" backgroundColor="#222222">
           <Text font="subtitle">Viewing Ads</Text>
           <Text font="headline">Ad Selection and Viewing by Ad Unit</Text>
           <Text font="normal">
-            After designing an ad using Thunder’s Creative Studio product, draft or final ad designs could be complied. Compiled ads have gone through some backend processing to ensure the best quality ad is displayed on the web or mobile device.
-          </Text>
-          <Text font="normal">
-            It was at this stage in the process where ThunderCast became a valuable tool for those users without access to creative studio or not directly involved in the ad design process. With the push of a button a designer could compile an ad and internal stakeholders or clients could offer feedback. When first signing into the app, it is this list of compiled ads users first see.
+            Blank
           </Text>
 
           <Text font="title">
             Viewing Ad Designs
           </Text>
           <Text font="normal">
-            Once an ad is selected from the the apps main landing screen, users can navigate to the main viewing experience. It was important the ad viewing experience not distract from the ad design itself. We solved for this by exposing app functionality and related metadata about the ad using an overlay menu design with opacities to reduce visual conflict.
+            Blank
           </Text>
           <ImageGallery maxItemsPerRow={2}>
             <Image src={Ads} maxWidth={375} maxHeight={667}/>
@@ -160,7 +240,7 @@ export const ThunderCast = (props: ThunderCastProps) => {
             Viewing True Render
           </Text>
           <Text font="normal">
-            Not only were users now able to view ads designs on the go, because they were viewing on a mobile device ads were now displayed at true native device DPI. This reduced some the challenges with viewing higher dpi ad designs within Thunder’s Creative Studio web editor that was limited to 72 and 96 browser dpi. In addition to viewing ad designs on the go, viewing on a mobile device was quickly creating unanticipated added value for our users. 
+            Blank
           </Text>
           <ImageGallery maxItemsPerRow={2}>
             <Image src={AdPreview_300x600} maxWidth={375} maxHeight={667}/>
@@ -171,7 +251,7 @@ export const ThunderCast = (props: ThunderCastProps) => {
             Viewing Larger Ad Units
           </Text>
           <Text font="normal">
-            Some larger format ad designs can encroach into the menu bars given mobile device’s small form factor. To solve for this we implemented a solution to hide the menu when a user taps on an ad design. Much like viewing an image in a photo app, simply swipe upwards and the user is returned back to the main ad design viewing experience.
+            Blank
           </Text>
           <ImageGallery maxItemsPerRow={2}>
             <Image src={AdPreview_300x600_TapZoom} maxWidth={375} maxHeight={667}/>
@@ -182,7 +262,7 @@ export const ThunderCast = (props: ThunderCastProps) => {
             Viewing Ad Design Details
           </Text>
           <Text font="normal">
-            Our new mobile app provided users the ability view ad designs at true size. Again given mobile device’s small form factor combined with an ad’s complexity, users sometimes found it difficult to view small important details. To solve for this we implemented a solution to zoom in on ad designs. Once a user has tapped to view an ad design, they can pinch to zoom and drag their finger to pan around the add design.
+            Blank
           </Text>
           <ImageGallery maxItemsPerRow={2}>
             <Image src={AdPreview_300x600_TapZoom} maxWidth={375} maxHeight={667}/>
@@ -193,10 +273,10 @@ export const ThunderCast = (props: ThunderCastProps) => {
             Viewing Ad Animations
           </Text>
           <Text font="normal">
-            Many HTML5 ads are designed with animation in mind. When viewing an animated ad design, the ad will animate the first time the ad loads. Users can replay the animation by clicking the large replay button in the center of the ad viewing menu. 
+            Blank
           </Text>
           <Text font="normal">
-            Given the relative shortness of most display ad animations, we opted for the simplicity of a replay button over a timeline animation scrubber interaction model. Designs also highlighted the replay capability because Thunder’s animated HTML5 ads were viewed as a differentiator against other ad design products.
+            Blank
           </Text>
           <ImageGallery maxItemsPerRow={2}>
             <Image src={AdPreview_300x250} maxWidth={375} maxHeight={667}/>
@@ -206,7 +286,7 @@ export const ThunderCast = (props: ThunderCastProps) => {
             Viewing Multiple Ad Sizes
           </Text>
           <Text font="normal">
-            Ads are ultimately made up of many different ad sizes, or what the Thunder Ad Framework refers to as ad units. We wanted to make it natural for users to quickly scan between ad units. Designs made this possible by swiping upwards, each swipe would advance the user to the next ad design.
+            Blank
           </Text>
           <ImageGallery maxItemsPerRow={2}>
             <Image src={AdPreview_300x250} maxWidth={375} maxHeight={667}/>
@@ -218,8 +298,8 @@ export const ThunderCast = (props: ThunderCastProps) => {
         </Section>
 
         <Section id="a" backgroundColor="#222222">
-          <Text font="subtitle">Commenting</Text>
-          <Text font="headline">Commenting Story Coming Soon</Text>
+          <Text font="subtitle">Ad Commenting</Text>
+          <Text font="headline">Contextual Comments</Text>
           <ImageGallery maxItemsPerRow={2}>
             <Image src={AdPreview_CommentMode} maxWidth={375} maxHeight={667}/>
             <Image src={AdPreview_CommentMode_ViewingComment} maxWidth={375} maxHeight={667} />
